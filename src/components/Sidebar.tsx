d54fb9ff905be151e23c2607003f0374d4f8d27e
jingjiang57323 <jingjiang57323@gmail.com>
2025-12-06 18:26:29 -0800

    new

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import insIcon from "./asset/sidebar/ins.png";
+import fbIcon from "./asset/sidebar/fb.png";
+import linkedinIcon from "./asset/sidebar/linkedin.png";
+import twitterIcon from "./asset/sidebar/twitter.png";
 
 export default function Sidebar() {
   const sideBar: React.CSSProperties = {
@@ -14,10 +18,10 @@ export default function Sidebar() {
   return (
     <div style={sideBar}>
       <p><b>Follow </b></p>
-      <a target="_blank" href="https://www.instagram.com/jjjjjjj11111119/"><img style={{"width":"30px"}} src="./pages/asset/sidebar/ins.png" alt="Instagram Icon"/></a>
-      <a target="_blank" href="https://www.facebook.com/jiang.rena.9/"><img style={{"width":"30px"}} src="./pages/asset/sidebar/fb.png" alt="Facebook Icon"/></a>
-      <a target="_blank" href="https://www.linkedin.com/in/jing-jiang08/"><img style={{"width":"30px"}} src="./pages/asset/sidebar/linkedin.png" alt="LinkedIn Icon"/></a>
-      <a target="_blank" href="https://x.com/JingJia83921054"><img style={{"width":"30px"}} src="./pages/asset/sidebar/twitter.png" alt="Twitter Icon"/></a>
+      <a target="_blank" href="https://www.instagram.com/jjjjjjj11111119/"><img style={{"width":"30px"}} src={insIcon} alt="Instagram Icon"/></a>
+      <a target="_blank" href="https://www.facebook.com/jiang.rena.9/"><img style={{"width":"30px"}} src={fbIcon} alt="Facebook Icon"/></a>
+      <a target="_blank" href="https://www.linkedin.com/in/jing-jiang08/"><img style={{"width":"30px"}} src={linkedinIcon} alt="LinkedIn Icon"/></a>
+      <a target="_blank" href="https://x.com/JingJia83921054"><img style={{"width":"30px"}} src={twitterIcon} alt="Twitter Icon"/></a>
     </div>
   );
 }
