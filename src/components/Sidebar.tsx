1453b1d1c38b59b1cdd82d39b1ab1965a57dd3ed
jingjiang57323 <jingjiang57323@gmail.com>
2025-12-06 17:00:28 -0800

    iiiiii

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,10 +14,10 @@ export default function Sidebar() {
   return (
     <div style={sideBar}>
       <p><b>Follow </b></p>
-      <a target="_blank" href="https://www.instagram.com/jjjjjjj11111119/"><img style={{"width":"30px"}} src="/asset/sidebar/ins.png" alt="Instagram Icon"/></a>
-      <a target="_blank" href="https://www.facebook.com/jiang.rena.9/"><img style={{"width":"30px"}} src="/asset/sidebar/fb.png" alt="Facebook Icon"/></a>
-      <a target="_blank" href="https://www.linkedin.com/in/jing-jiang08/"><img style={{"width":"30px"}} src="/asset/sidebar/linkedin.png" alt="LinkedIn Icon"/></a>
-      <a target="_blank" href="https://x.com/JingJia83921054"><img style={{"width":"30px"}} src="/asset/sidebar/twitter.png" alt="Twitter Icon"/></a>
+      <a target="_blank" href="https://www.instagram.com/jjjjjjj11111119/"><img style={{"width":"30px"}} src="./pages/asset/sidebar/ins.png" alt="Instagram Icon"/></a>
+      <a target="_blank" href="https://www.facebook.com/jiang.rena.9/"><img style={{"width":"30px"}} src="./pages/asset/sidebar/fb.png" alt="Facebook Icon"/></a>
+      <a target="_blank" href="https://www.linkedin.com/in/jing-jiang08/"><img style={{"width":"30px"}} src="./pages/asset/sidebar/linkedin.png" alt="LinkedIn Icon"/></a>
+      <a target="_blank" href="https://x.com/JingJia83921054"><img style={{"width":"30px"}} src="./pages/asset/sidebar/twitter.png" alt="Twitter Icon"/></a>
     </div>
   );
 }
